2ffd15dcee14eaf730a9a992b35439aac1324e32
Kyuu <Public.Kyuuanago@gmail.com>
2019-04-11 18:55:40 +0900

    I do nothing

--- Console/converter.kt
+++ Console/converter.kt
@@ -1,5 +1,4 @@
-/*
-    /func(...)
+/*func(...)
     ____...
 
     etc...
@@ -8,27 +7,30 @@
 import java.util.Stack
 import java.io.Reader
 import java.io.BufferedReader
-import java.io.StringBuilder
 
 public class Converter {
 
-    private val commandStack: Stack<String>
+    private val commandStack: Stack<Int>
     private var tempList = StringBuilder()
 
     init {
-        commandStack = Stack<Stirng>()
+        commandStack = Stack<Int>()
     }
     
     public fun start(r: Reader): String {
         val reader = BufferedReader(r)
         var output = StringBuilder()
         while(reader.ready()) {
-            output.append(convert(reader.read()))
+            output.append(convert(reader.readLine()))
         }
         return output.toString()
     }
 
-    private fun convert(input: char): String {
-        
+    private fun convert(input: String): String {
+        if("\\func" in input) {
+            commandStack.push(input.length - input.trimStart().length)
+            print("${input.length - input.trimStart().length}")
+        }   
+        return input
     }
 }
